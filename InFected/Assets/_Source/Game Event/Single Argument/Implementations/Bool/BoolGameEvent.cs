b5884ac6b78dcedee6bbcfc0db9714e3a2754306
MatveyMironov <matveymironof@gmail.com>
2026-04-08 10:41:08 +0300

    Game event system naming and foldering

--- InFected/Assets/_Source/Game Event/Single Argument/Implementations/Bool/BoolGameEvent.cs
+++ InFected/Assets/_Source/Game Event/Single Argument/Implementations/Bool/BoolGameEvent.cs
@@ -2,6 +2,6 @@ using UnityEngine;
 
 namespace GameEventSystem
 {
-    [CreateAssetMenu(fileName = "New Bool Game Event", menuName = "Bool Game Event")]
+    [CreateAssetMenu(fileName = "New Bool Game Event", menuName = "Game Event/Bool")]
     public class BoolGameEvent : AGameEvent<bool> { }
 }
--- InFected/Assets/_Source/Game Event/Single Argument/Implementations/String/StringGameEvent.cs
+++ InFected/Assets/_Source/Game Event/Single Argument/Implementations/String/StringGameEvent.cs
@@ -2,6 +2,6 @@ using UnityEngine;
 
 namespace GameEventSystem
 {
-    [CreateAssetMenu(fileName = "New String Game Event", menuName = "String Game Event")]
+    [CreateAssetMenu(fileName = "New String Game Event", menuName = "Game Event/String")]
     public class StringGameEvent : AGameEvent<string> { }
 }
